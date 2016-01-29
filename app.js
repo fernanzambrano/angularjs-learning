400ebf53b70e32a34951735c839a69def1351746
Fernando <fernanzambrano@gmail.com>
2016-01-29 21:19:50 +0100

    Directive ng-src

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 (function(){
 	
  	var gems = [
- 				{ name: 'Azurite 1', price: 3.00, visible: true, canPurchase: false, dateInput: '1452950043541' },
- 				{ name: 'Azurite 2', price: 4.00, visible: true, canPurchase: true, dateInput: '1452950043541' },
- 				{ name: 'Azurite 3', price: 2.00, visible: false, canPurchase: true, dateInput: '1452950043541' },
- 				{ name: 'Azurite 4', price: 1.00, visible: true, canPurchase: false, dateInput: '1452950043541' }
+ 				{ name: 'Azurite 1', price: 3.00, visible: true, 
+ 				  canPurchase: false, dateInput: '1452950043541',
+ 				  image: 'images/gems1.png' },
+ 				{ name: 'Azurite 2', price: 4.00, visible: true, 
+ 				  canPurchase: true, dateInput: '1452950043541',
+ 				  image: 'images/gems2.png' },
+ 				{ name: 'Azurite 3', price: 2.00, visible: false, 
+ 				  canPurchase: true, dateInput: '1452950043541' },
+ 				{ name: 'Azurite 4', price: 1.00, visible: true, 
+ 				  canPurchase: false, dateInput: '1452950043541', 
+ 				  image: 'images/gems1.png' }
  			   ];
 
  	var app = angular.module('gemStore', []);
